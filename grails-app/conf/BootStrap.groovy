04e2c97d1cc599632ce33bd775f0f13ce1535479
L3TZ <thomas_zoratto@hotmail.fr>
2015-04-08 14:03:57 +0200

    invocation du service du jeu de test dans le bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,12 @@
+import toulousemusee.JeuTestMuseeService
+
 class BootStrap {
 
+    JeuTestMuseeService jeuTestMuseeService
+
     def init = { servletContext ->
+
+        jeuTestMuseeService.createJeuTest()
     }
     def destroy = {
     }
