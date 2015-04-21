d595dbdf9c13e0d8e02d9c28ff11d055b710bbd5
lpierillas <lucie.pierillas@gmail.com>
2015-04-21 21:22:51 +0200

    TzorLpie-ToulouseMusee-7 Test unitaires close #7 test pour suiviDemandesController

--- test/unit/toulousemusee/SuiviDemandesControllerSpec.groovy
+++ test/unit/toulousemusee/SuiviDemandesControllerSpec.groovy
@@ -1,5 +1,6 @@
 package toulousemusee
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,14 +8,42 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(SuiviDemandesController)
+@Mock([DemandeVisite])
 class SuiviDemandesControllerSpec extends Specification {
 
+    def demande
+
     def setup() {
+        demande = new DemandeVisite(dateDebutPeriode: new Date(2015,8,16),
+                dateFinPeriode:new Date(2015,8,17),
+                nbPersonnes: 3,
+                dateDemande: new Date(),
+                statut: 'En cours de traitement',
+                code: UUID.randomUUID().toString()).save()
     }
 
-    def cleanup() {
+    void "Test the suiviDemande action with a valid code returns the correct model"() {
+
+        given:"Un code de demande valide"
+        controller.params.codeDemande = demande.code
+
+        when:"The suiviDemande action is executed"
+        controller.suiviDemande()
+
+        then:
+        model.infoDemande==demande
     }
 
-    void "test something"() {
+    void "Test the suiviDemande action with an invalid code returns the correct model"() {
+
+        given:"Un code de demande valide"
+        controller.params.codeDemande = 5
+
+        when:"The suiviDemande action is executed"
+        controller.suiviDemande()
+
+        then:
+        model.retourFormulaire == 'Le code n\'est pas valide.'
     }
+
 }
